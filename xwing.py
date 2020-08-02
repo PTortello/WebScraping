beff98e0b4ee2b955bdc6fb03d71f8844b2f0dfa
Pedro Tortello <ptortello@hotmail.com>
2020-08-02 20:57:41 -0300

    File Handling

--- xwing.py
+++ xwing.py
@@ -1,27 +1,29 @@
+# Listfortress is a website containing a list of X-Wing tournaments
+
 from bs4 import BeautifulSoup
 import requests
 
-page = 1
-#while(True):
-for k in range(1,2):
-    param = {'page': page}
-    address = 'https://listfortress.com/tournaments'
-
-    req = requests.get(address, param)
-    soup = BeautifulSoup(req.text, 'lxml')
-
-    #tournament = soup.tbody.find('tr')
-    for tournament in soup.tbody.find_all('tr'):
-        for info in tournament.find_all('td'):
-            print(info.text)
-
-    #page += 1
-
-"""
-data = ['aaa', 'bbb', 'ccc']
 with open('scrap.csv', 'w') as f:
-    f.write('START\n\n')
-    for d in data:
-        f.write(d + ',')
-    f.write('\n\nEND')
-"""
+
+    page = 1
+    #while(True):
+    for k in range(1,2):
+        param = {'page': page}
+        address = 'https://listfortress.com/tournaments'
+
+        req = requests.get(address, param)
+        soup = BeautifulSoup(req.text, 'lxml')
+
+        #tournament = soup.tbody.find('tr')
+        for tournament in soup.tbody.find_all('tr'):
+            i = 0
+            for info in tournament.find_all('td'):
+                #print(info.text)
+                f.write(info.text + ',')
+                i += 1
+                if i == 8:
+                    f.write('\n')
+                    break
+
+        #page += 1
+# lines = open( 'sta1214.txt', "r" ).readlines()[::2]
