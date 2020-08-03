98ea89f0039c8d603195d6731639a904ab66e795
Pedro Tortello <ptortello@hotmail.com>
2020-08-02 22:03:08 -0300

    Extra lines removed from csv

--- xwing.py
+++ xwing.py
@@ -3,11 +3,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-with open('scrap.csv', 'w') as f:
+with open('scrap.csv', 'w', encoding='utf-8') as f:
 
     page = 1
     #while(True):
-    for k in range(1,2):
+    for k in range(1,58):
+        print('Scraping page ' + str(page) + ' ...')
         param = {'page': page}
         address = 'https://listfortress.com/tournaments'
 
@@ -16,14 +17,20 @@ with open('scrap.csv', 'w') as f:
 
         #tournament = soup.tbody.find('tr')
         for tournament in soup.tbody.find_all('tr'):
-            i = 0
+            column = 0
             for info in tournament.find_all('td'):
-                #print(info.text)
-                f.write(info.text + ',')
-                i += 1
-                if i == 8:
-                    f.write('\n')
-                    break
+                f.write(info.text)
+                if column < 7:
+                    f.write(',')
+                column += 1
 
-        #page += 1
-# lines = open( 'sta1214.txt', "r" ).readlines()[::2]
+        page += 1
+
+# Removes one from every two lines
+print('Removing lines ...')
+lines = open('scrap.csv', 'r', encoding='utf-8').readlines()[::2]
+with open('scrap.csv', 'w', encoding='utf-8') as f:
+    for i in lines:
+        f.write(i)
+
+print('Finished')
