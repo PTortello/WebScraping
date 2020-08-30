84ea18272d2668a09b977dc345f6c6f2154b5b35
Pedro Tortello <ptortello@hotmail.com>
2020-08-30 18:33:34 -0300

    Comments added

--- temperature/tempscrap.py
+++ temperature/tempscrap.py
@@ -1,14 +1,22 @@
+"""
+tempscrap.py
+Pedro Tortello - 08/2020
+Current temperature web scraping for choosen location.
+"""
+
 from bs4 import BeautifulSoup
 from requests import get
 
-
+# CAUTION: Hard coded file name containing url address
 source = "sorocaba.dat"
 with open(source, 'r') as data:
     address = data.read()
 
+# Web scraping url address
 req = get(address).text
 soup = BeautifulSoup(req, 'lxml')
 
+# Searching current temperature
 match = soup.find('div', id='mainContent')
 match = match.find_all('span')
 match = match[7].text[1:3]
